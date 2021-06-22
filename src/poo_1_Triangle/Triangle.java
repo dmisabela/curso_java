be5d1ad2c7e71d8ca47e7434a85a2088fc39cce0
Isabela Duarte <duartemorenoisabela@gmail.com>
2021-06-21 21:20:19 -0300

    method calcularArea

--- src/poo_1_Triangle/Triangle.java
+++ src/poo_1_Triangle/Triangle.java
@@ -1,9 +1,27 @@
-package poo_1_Triangle;
+package poo_1_Triangle; //pacote
 
-public class Triangle {
+public class Triangle { //nome da classe
 	
-	public double a;
-	public double b;
+	public double a; //atributos
+	public double b; //public: atributo ou método pode ser usado em outros arquivos
 	public double c;
-
+	
+	
+	public double calcularArea() { //método  
+		
+		
+		//double: tipo do dado que o método retorna
+		//se não retorna nada é void
+		//está assim () pois não precisa de nenhum parâmetro além dos que já estão na classe
+		
+		double p = (a + b + c)/2.0;
+		
+		double result =  Math.sqrt(p * (p - a)  * (p - b) * (p - c));
+		
+		return result;
+		
+	}
+	
+	
+	
 }
